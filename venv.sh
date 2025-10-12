375cffa5e76abcd0e98f9f327f843cfda3de9813
Hyp3rstatic <kazjrosinski@gmail.com>
2025-10-12 14:31:38 -0700

    improved venv script

--- venv.sh
+++ venv.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-python3 -m venv ./venv/
+PROJECT_DIR=$(realpath "${BASH_SOURCE[0]}"/..)
 
-source ./venv/bin/activate
+NEW_VENV=0 #false
 
-pip install -r requirements.txt
+if [[ ! -d $PROJECT_DIR/venv ]]; then
+    echo $PROJECT_DIR/venv
+    python3 -m venv $PROJECT_DIR/venv
+    ((NEW_VENV=1))
+fi
+
+if [[ $VIRTUAL_ENV != $PROJECT_DIR/venv ]]; then
+    source $PROJECT_DIR/venv/bin/activate
+fi
+
+PACKAGE_COUNT=$(pip list | wc -l)
+((PACKAGE_COUNT-=2))
+
+if [[ PACKAGE_COUNT -le 1 || NEW_VENV -eq 1 ]]; then
+    pip install -r requirements.txt
+fi
