782ddbd616225abf9656629f734c0c2913871e32
Hyp3rstatic <kazjrosinski@gmail.com>
2025-10-12 14:03:36 -0700

    get fit data

--- src/miri.py
+++ src/miri.py
@@ -1,4 +1,6 @@
 from astroquery.mast import Observations
+from astropy.io import fits
+import os
 
 #https://mast.stsci.edu/portal/Mashup/Clients/Mast/Portal.html
 #Go to MAST Observations by Advanced instrument: MIRI/SLIT target_classification: Exoplanets, select planet
@@ -7,16 +9,31 @@ from astroquery.mast import Observations
 #Downloading _x1d.fits data: 
 #Target Exoplanets
 
-#contains one entry
-obs_table = Observations.query_criteria(
-    instrument_name="MIRI/SLIT",
-    target_name="GU-PSC-B",
-)
+projectroot = os.path.abspath(__file__+"/../..")
+planetfolder = projectroot+"/planetdata"
+gupscb = planetfolder+"/GU-PSC-B"
 
-product_list = Observations.get_product_list(obs_table)
+if not os.path.exists(gupscb):
 
-manifest = Observations.download_products(product_list,
-                                          extension="_x1d.fits",
-                                          download_dir='./planetdata/GU-PSC-B'
-                                          )
+    #table contains one entry
+    obs_table = Observations.query_criteria(
+        instrument_name="MIRI/SLIT",
+        target_name="GU-PSC-B",
+    )
 
+    product_list = Observations.get_product_list(obs_table)
+
+    filtered_products = Observations.filter_products(product_list, extension="_x1d.fits")
+
+    manifest = Observations.download_products(filtered_products, download_dir=planetfolder)
+
+    os.rename(planetfolder+"/mastDownload/JWST/", gupscb)
+
+    os.removedirs(planetfolder+"/mastDownload")
+
+if os.path.exists(gupscb+"/jw01188-o011_t003_miri_p750l/jw01188-o011_t003_miri_p750l_x1d.fits"):
+    with fits.open(gupscb+"/jw01188-o011_t003_miri_p750l/jw01188-o011_t003_miri_p750l_x1d.fits") as hdul:
+        hdul.info()
+        print(hdul[0].data)
+        print(hdul[1].data)
+        print(hdul[2].data)
